04a4e592f7c35ec62932d93a082ea3a612815c42
arunjathu <arun.jathavedan7@gmail.com>
2020-11-28 17:20:08 +0530

    Update python_yaml.py

--- python_yaml.py
+++ python_yaml.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
 
-
 import yaml
 import os
-
-file = os.stat('test.yml').st_size
+import subprocess
 
 finame = input('first name ')
 liname = input('last name ')
 iage = int(input('age '))
 
+if os.path.isfile('test.yml'):
+    file = os.stat('test.yml').st_size
 
-if file == 0:
-  person = dict(users=[dict(fname=finame, lname=liname, age=iage),])
-  with open('test.yml', 'a') as outfile:
-    yaml.dump(person, sort_keys=False, stream=outfile, default_flow_style=False, indent=4)
+    if file == 0:
+        person = dict(users=[dict(userid=1,fname=finame, lname=liname, age=iage),])
+        with open('test.yml', 'a') as outfile:
+            yaml.dump(person, stream=outfile, default_flow_style=False, sort_keys=False, indent=4)
 
-    
+
+    else:
+        with open('test.yml', 'a+') as outfile:
+               pt=yaml.load(outfile,Loader=yaml.FullLoader)
+               num=len(pt['users'])
+
+        persons = [dict(userid=num+1,fname=finame, lname=liname, age=iage),]
+        with open('test.yml', 'a') as outfile:
+            yaml.dump(persons, stream=outfile, default_flow_style=False, sort_keys=False, indent=4)
+
+
+
 else:
-  persons = [dict(fname=finame, lname=liname, age=iage),]
-  with open('test.yml', 'a') as outfile:
-    yaml.dump(persons, sort_keys=False, stream=outfile, default_flow_style=False, indent=4)
+    os.mknod("test.yml")
+
+    person = dict(users=[dict(userid=1,fname=finame, lname=liname, age=iage),])
+    with open('test.yml', 'a') as outfile:
+        yaml.dump(person, stream=outfile, default_flow_style=False, sort_keys=False, indent=4)
