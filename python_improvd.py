336ff0227b3756db2b75a278715f4bea4048ba96
arunjathu <arun.jathavedan7@gmail.com>
2020-11-28 16:39:23 +0530

    Update python_improvd.py

--- python_improvd.py
+++ python_improvd.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-
-
-
 import yaml
 import os
 import subprocess
@@ -12,21 +9,28 @@ liname = raw_input('last name ')
 iage = int(raw_input('age '))
 
 if os.path.isfile('test.yml'):
- file = os.stat('test.yml').st_size
+    file = os.stat('test.yml').st_size
 
- if file == 0:
-  person = dict(users=[dict(fname=finame, lname=liname, age=iage),])
-  with open('test.yml', 'a') as outfile:
-    yaml.dump(person, stream=outfile, default_flow_style=False, indent=4)
+    if file == 0:
+        person = dict(users=[dict(unique=1,fname=finame, lname=liname, age=iage),])
+        with open('test.yml', 'a') as outfile:
+            yaml.dump(person, stream=outfile, default_flow_style=False, indent=4)
 
 
- else:
-  persons = [dict(fname=finame, lname=liname, age=iage),]
-  with open('test.yml', 'a') as outfile:
-    yaml.dump(persons, stream=outfile, default_flow_style=False, indent=4)
+    else:
+        with open('test.yml', 'a+') as outfile:
+               pt=yaml.safe_load(outfile)
+               num=len(pt['users'])
+
+        persons = [dict(unique=num+1,fname=finame, lname=liname, age=iage),]
+        with open('test.yml', 'a') as outfile:
+            yaml.dump(persons, stream=outfile, default_flow_style=False, indent=4)
+
+
 
 else:
- os.mknod("test.yml")
- person = dict(users=[dict(fname=finame, lname=liname, age=iage),])
- with open('test.yml', 'a') as outfile:
-    yaml.dump(person, stream=outfile, default_flow_style=False, indent=4)
+    os.mknod("test.yml")
+
+    person = dict(users=[dict(unique=1,fname=finame, lname=liname, age=iage),])
+    with open('test.yml', 'a') as outfile:
+        yaml.dump(person, stream=outfile, default_flow_style=False, indent=4)
